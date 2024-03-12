b6b5be82310c598dc85900b9df537a41d7c514f8
JuliaRC <jubjub.chan@gmail.com>
2024-03-12 18:17:40 +0000

    added-button

--- script.js
+++ script.js
@@ -5,4 +5,15 @@ $(document).ready(function (){
     $("#abrirAnimal").click(function (){
         $('#exampleModalCenter').modal('show');
     })
+
+    let applyButton = document.querySelector(".buttonForm");
+    applyButton.addEventListener("click", more);
 })
+
+function more() {
+    let firstName = prompt("What is your first name?");
+    let email = prompt("What is your email address?");    
+    alert("Hi " + firstName + "! Welcome to Happy Paws, we will be in touch soon 🤗");
+}
+
+         
